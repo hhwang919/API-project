eddfbbaa27976300f51fe61e240cc3fa7e659a2c
Hubert Hwang <huberthwang919@gmail.com>
2023-01-24 14:59:48 -0800

    work in progress

---
+++ backend/utils/fakerSeed.js
@@ -0,0 +1,38 @@
+const { faker } = require('@faker-js/faker');
+const bcrypt = require('bcryptjs');
+
+//simple random number generator
+const rNum = (num) => Math.floor(Math.random() * Math.floor(num) + 1)
+
+
+const seedUsers = (num) => {
+    let users = new Array(num).fill('')
+
+for (const i in users) {
+    users[i] = {
+        username: faker.internet.userName(),
+        email: faker.internet.email() ,
+        hasedPassword: bcrypt.hashSync(faker.internet.password())
+    }
+    while(users[i].username.length > 30){
+        users[i] = {
+            username: faker.internet.username(),
+            email: faker.internet.email(),
+            hashedPassword: bcrypt.hashSync(faker.internet.password())
+        }
+    }
+}
+
+return users
+}
+
+
+module.exports ={
+
+    seedUsers
+
+}
+
+
+
+//// lets uers seedUsers(100)
