1cdfbb0ba8f87d040f8e3e2e90af7a4df105da79
Hubert Hwang <huberthwang919@gmail.com>
2023-01-24 11:02:33 -0800

    refactor create-user migrations

--- backend/db/migrations/20230121003528-create-user.js
+++ backend/db/migrations/20230121003528-create-user.js
@@ -16,12 +16,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      firstName:{
-        type: Sequelize.STRING
-      },
-      lastName:{
-        type: Sequelize.STRING
-      },
       username: {
         type: Sequelize.STRING(30),
         allowNull: false,
