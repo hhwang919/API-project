b414c7e39e8a4d2e721a2dbcae4160c20b08709e
Hubert Hwang <huberthwang919@gmail.com>
2023-01-24 16:29:41 -0800

    create bookings model

---
+++ backend/db/models/bookings.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Bookings extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Bookings.init({
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Bookings',
+  });
+  return Bookings;
+};
