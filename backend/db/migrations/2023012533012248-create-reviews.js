6b0a9ff47f3f3c250a87568f90b67e285f8a51b0
Hubert Hwang <huberthwang919@gmail.com>
2023-02-21 21:10:16 -0800

    testing references

--- backend/db/migrations/2023012533012248-create-reviews.js
+++ backend/db/migrations/2023012533012248-create-reviews.js
@@ -19,12 +19,16 @@ module.exports = {
       spotId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: { model: 'Spots' },
+        references: { 
+          model: 'Spots',
+          key: 'id' },
       },
       userId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: { model: 'Users' },
+        references: { 
+          model: 'Users',
+          key: 'id' },
       },
       review: {
         type: Sequelize.TEXT
