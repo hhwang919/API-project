d6cf818ed5e5f63d4de22850323d3478c6084243
Hubert Hwang <huberthwang919@gmail.com>
2023-03-13 11:06:55 -0700

    logout frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+
 
 function App() {
   const dispatch = useDispatch();
@@ -10,11 +12,15 @@ function App() {
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
+console.log("This isLoaded:", isLoaded)
 
   return isLoaded && (
     <Switch>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -60,8 +60,19 @@ export const signup = (user) => async (dispatch) => {
     return response;
   };
 
+//logout
 
-const initialState = { user: null };
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
+
+  const initialState = { user: null };
+
 
 const sessionReducer = (state = initialState, action) => {
   let newState;
