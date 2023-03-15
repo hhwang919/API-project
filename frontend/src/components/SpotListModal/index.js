01791cf4a45d8d84714d5b36a81696494ac3f11b
Hubert Hwang <huberthwang919@gmail.com>
2023-03-15 02:34:57 -0700

    spot list css

--- frontend/src/components/SpotListModal/index.js
+++ frontend/src/components/SpotListModal/index.js
@@ -6,6 +6,7 @@ import { fetchSpots } from '../../store/spotReducer';
 import { useModal } from "../../context/Modal";
 import * as spotActions from "../../store/spotReducer"
 // import * as sessionActions from "../../store/session";
+import './SpotTile.css';
 
 
 import React from 'react';
@@ -23,24 +24,43 @@ const SpotListModal = () => {
   useEffect(() => {
     dispatch(fetchSpots());
   }, [dispatch]);
+  
+  const Tile = ({spot}) => {
+    return (
+        <div className='spot-tile'>
+      <div className="tile">
+        <img src={spot.previewImage} alt={spot.city} />
+        <div className="tile-text">
+          <h3>{spot.city}, {spot.state}</h3>
+          <p>{spot.description}</p>
+          <NavLink to={`/spot/${spot.id}`} className="btn">View Details</NavLink>
+        </div>
+      </div>
+        </div>
+    )
+  }
+
   return (
     <div>
-      <h1>Spot List</h1>
-      <ol>
-        {Object.values(spots).map(spot => (
-          <li key={spot.id}>
-            <NavLink to={`/spot/${spot.id}`}>{spot.city}, {spot.state}</NavLink>
-          </li>
-        ))}
-      </ol>
+        <h1>Spot List</h1>
+    <div className='spot-header'>
+       <ul>
 
+        {Object.values(spots).map(spot => (
+            <Tile key={spot.id} spot={spot} />
+            // {/* <NavLink to={`/spot/${spot.id}`}>{spot.city}, {spot.state}</NavLink> */}  
+            ))}
+            </ul>
+        
+
+            </div>
+            </div>
+          );
       {/*<Switch>
         <Route path='/spot/:id'>
-          <SingleSpot spots={spots.id} />
+        <SingleSpot spots={spots.id} />
         </Route>
-        </Switch>*/}
-    </div>
-  );
+    </Switch>*/}
 }
 
 export default SpotListModal;
